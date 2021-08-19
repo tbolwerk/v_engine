9c4612e92c2d13d65cc2ca65752ef261d44c5897
twanbolwerk <twanbolwerk@gmail.com>
2021-08-19 11:57:22 +0200

    index buffers added, performance increases when loading static obj

--- model.hpp
+++ model.hpp
@@ -22,7 +22,13 @@ namespace v_engine
             static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
         };
 
-        Model(Device &device, const std::vector<Vertex> &vertices);
+        struct Builder
+        {
+            std::vector<Vertex> vertices{};
+            std::vector<uint32_t> indices{};
+        };
+
+        Model(Device &device, const Model::Builder &builder);
         ~Model();
         
         Model(const Model &) = delete;
@@ -39,6 +45,12 @@ namespace v_engine
         VkDeviceMemory vertexBufferMemory;
         uint32_t vertexCount;
 
+        bool hasIndexBuffer = false;
+        VkBuffer indexBuffer;
+        VkDeviceMemory indexBufferMemory;
+        uint32_t indexCount;
+
         void createVertexBuffers(const std::vector<Vertex> &vertices);
+        void createIndexBuffers(const std::vector<uint32_t> &indices);
     };
 }
