808bedb399bd7a7c276451a61e47fd9c139c1af2
twanbolwerk <twanbolwerk@gmail.com>
2021-08-19 12:50:51 +0200

    reducing vertices count from (30888) to (5546) for smooth vase

--- model.cpp
+++ model.cpp
@@ -1,4 +1,26 @@
 #include "model.hpp"
+#include "utils.hpp"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "libs/tiny_obj_loader.h"
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+
+#include <cassert>
+#include <cstring>
+#include <iostream>
+#include <unordered_map>
+
+namespace std{
+    template <>
+    struct hash<v_engine::Model::Vertex> {
+        size_t operator()(v_engine::Model::Vertex const &vertex) const{
+            size_t seed = 0;
+            v_engine::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+            return seed;
+        }
+    };
+}
 
 namespace v_engine
 {
@@ -19,6 +41,14 @@ namespace v_engine
             vkDestroyBuffer(device.device(), indexBuffer, nullptr);
             vkFreeMemory(device.device(), indexBufferMemory, nullptr);
         }
+    }
+
+    std::unique_ptr<Model> Model::createModelFromFile(Device &device, const std::string &filePath)
+    {
+        Builder builder{};
+        builder.loadModel(filePath);
+        std::cout << "Vertex count: " << builder.vertices.size() << std::endl;
+        return std::make_unique<Model>(device, builder);
     }
 
     void Model::createVertexBuffers(const std::vector<Vertex> &vertices)
@@ -139,5 +169,73 @@ namespace v_engine
         attributeDescription[1].offset = offsetof(Vertex, color);
 
         return attributeDescription;
+    }
+
+    void Model::Builder::loadModel(const std::string &filePath)
+    {
+        tinyobj::attrib_t attrib;
+        std::vector<tinyobj::shape_t> shapes;
+        std::vector<tinyobj::material_t> materials;
+        std::string warn, err;
+
+        if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filePath.c_str()))
+        {
+            throw std::runtime_error(warn + err);
+        }
+
+        vertices.clear();
+        indices.clear();
+
+std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+        for (const auto &shape : shapes)
+        {
+            for (const auto &index : shape.mesh.indices)
+            {
+                Vertex vertex{};
+                if (index.vertex_index >= 0)
+                {
+                    vertex.position = {
+                        attrib.vertices[3 * index.vertex_index + 0],
+                        attrib.vertices[3 * index.vertex_index + 1],
+                        attrib.vertices[3 * index.vertex_index + 2]
+
+                    };
+
+                    auto colorIndex = 3 * index.vertex_index + 2;
+                    if(colorIndex < attrib.colors.size())
+                    {
+                        vertex.color = {
+                            attrib.colors[colorIndex - 2],
+                            attrib.colors[colorIndex - 1],
+                            attrib.colors[colorIndex - 0]
+                        };
+                    }else{
+                        vertex.color = {1.f, 1.f, 1.f};
+                    }
+                }
+                if (index.normal_index >= 0)
+                {
+                    vertex.normal = {
+                        attrib.vertices[3 * index.normal_index + 0],
+                        attrib.vertices[3 * index.normal_index + 1],
+                        attrib.vertices[3 * index.normal_index + 2]
+
+                    };
+                }
+                if (index.texcoord_index >= 0)
+                {
+                    vertex.uv = {
+                        attrib.vertices[2 * index.texcoord_index + 0],
+                        attrib.vertices[2 * index.texcoord_index + 1]
+                    };
+                }
+                if(uniqueVertices.count(vertex) == 0)
+                {
+                    uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+                    vertices.push_back(vertex);
+                }
+                indices.push_back(uniqueVertices[vertex]);
+            }
+        }
     }
 }
--- model.hpp
+++ model.hpp
@@ -7,6 +7,7 @@
 #include <glm/glm.hpp>
 
 #include <vector>
+#include <memory>
 
 namespace v_engine
 {
@@ -17,15 +18,23 @@ namespace v_engine
         {
             glm::vec3 position{};
             glm::vec3 color{};
+            glm::vec3 normal{};
+            glm::vec2 uv{};
 
             static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
             static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+
+            bool operator==(const Vertex &other) const
+            {
+                return position == other.position && color == other.color && normal == other.normal && uv == other.uv;
+            }
         };
 
         struct Builder
         {
             std::vector<Vertex> vertices{};
             std::vector<uint32_t> indices{};
+            void loadModel(const std::string &filePath);
         };
 
         Model(Device &device, const Model::Builder &builder);
@@ -34,7 +43,7 @@ namespace v_engine
         Model(const Model &) = delete;
         Model &operator=(const Model &) = delete;
 
-        
+        static std::unique_ptr<Model> createModelFromFile(Device &device, const std::string &filePath);
 
         void bind(VkCommandBuffer commandBuffer);
         void draw(VkCommandBuffer commandBuffer);
